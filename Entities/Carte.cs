bb904431ed097463823c7f0152d096f914e41747
AnthoninHelias <heliasanthonin@yahoo.fr>
2024-11-07 15:19:44 +0100

    commit d'intiliasation

---
+++ Entities/Carte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Entities;
+
+public partial class Carte
+{
+    public int Id { get; set; }
+
+    public string Nom { get; set; } = null!;
+
+    public string Rareté { get; set; } = null!;
+}
---
+++ Entities/ProjetGestionContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities;
+
+public partial class ProjetGestionContext : DbContext
+{
+    public ProjetGestionContext()
+    {
+    }
+
+    public ProjetGestionContext(DbContextOptions<ProjetGestionContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Carte> Cartes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=;database=Projet_gestion");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Carte>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("Cartes", "Projet_gestion");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Nom).HasMaxLength(100);
+            entity.Property(e => e.Rareté).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
